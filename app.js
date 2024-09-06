0a8e5df741dc133dd43123f7ad43524b9fd60ac9
bek-211203-coder <bek406197@gmail.com>
2024-09-06 18:45:58 +0500

    setInterval va setTimeout

---
+++ app.js
@@ -0,0 +1,18 @@
+// console.log('salom');
+// setTimeout(() => {
+//     console.log('salom bolalar');
+// }, 5000);
+/* bu aperatsiyada salom darhol consolda korinadi lekin  salom bolalar sozi  5 soniyadan song korinadi  demak setTimeout bizga consolga *     chiqarishni malum soniyalar yoki vatga kechiktirib beradi deb tariflasak boladi */
+
+/////////////////////
+
+// let count = 0;
+// const value = setInterval(() => {
+//     count++;
+//     console.log(`Count: ${count}`);
+//     if (count >= 5) {
+//         clearInterval(value);
+//         console.log('dastur yakunlandi');
+//     }
+// }, 1000);
+/*bu yerda  setInterval harbir soniyada countni 1 ga oshirib boradi va bu holat tohtovsiz amalga oshiriladi biz uni ozimiz toxtatmaguncha bu holatda setInterval 5 ga borib tohtaydi chunki biz sharta count 5dan kotta yoki teng bolsa maxsus clearInterval codi yordamida dasdurni toxtatyabmiz va consolga dastur yakunlandi deb chiqaryabmiz   */ 
